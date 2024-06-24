ee7b1b9c46505c479775fcbb61e4da8ca2273ba6
Eunice Dickson <sunshinebae439@gmail.com>
2024-06-24 18:31:43 +0000

    Imported the icon images, and created the profile header, added the bank card and added the task icons

--- FinTrack/screens/HomeScreen.js
+++ FinTrack/screens/HomeScreen.js
@@ -1,17 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, Text, Image, Pressable} from 'react-native';
 import styles from '../styles';
 
 const HomeScreen = () => {
-return( 
-  <View style={styles.container}>
-    <StatusBar barStyle="dark-content"/>
-    <View style={styles.homeHeader}>
-      
+  const Profile = require('../assets/profile.png');
+  const Search = require('../assets/search.png');
+  const bankCard = require('../assets/Card.png');
+  const send = require('../assets/send.png');
+  const receive = require('../assets/recieve.png');
+  const loan = require('../assets/loan.png');
+  const topUp = require('../assets/topUp.png');
+  const appleIcon = require('../assets/apple.png');
+  const spotify = require('../assets/spotify.png');
+  const transfer = require('../assets/moneyTransfer.png');
+  return( 
+    <View style={styles.container}>
+      <StatusBar barStyle="dark-content"/>
+      <View style={styles.homeHeader}>
+        <Image style={styles.Profile} source={Profile} />
+        <View style={styles.headerText}>
+          <Text style={styles.homeHeaderText}>Welcome back,</Text>
+          <Text style={styles.homeHeaderText2}>Eric Atsu</Text>
+        </View>
+        <Pressable style={styles.searchBackground}>
+          <Image style={styles.searchIcon} source={Search}/>
+        </Pressable>
+      </View>
+      <View style={styles.bankCard}>
+        <Image style={styles.card} source={bankCard}/>
+      </View>
+      <View style={styles.status}>
+        <Pressable style={styles.statusIcon}>
+          <Image source={send}/>
+        </Pressable>
+        <Pressable style={styles.statusIcon}> 
+          <Image source={receive}/>
+        </Pressable>
+        <Pressable style={styles.statusIcon}>
+          <Image source={loan}/>
+        </Pressable >
+        <Pressable style={styles.statusIcon}>
+          <Image source={topUp}/>
+        </Pressable>
+      </View>
+      <View style={styles.transactions}>
+        <View style={styles.transactionsHeader}>
+          <Text style={styles.transactionsHeaderText}>Transaction</Text>
+          <Text style={styles.transactionsHeaderLink}>Sell All</Text>
+        </View>
+      </View>
     </View>
-  </View>
-)
+  )
 }
 
 export default HomeScreen;
--- FinTrack/styles.js
+++ FinTrack/styles.js
@@ -2,12 +2,84 @@ import React from "react";
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
+  //For the Home screen
   container: {
-    backgroundColor: '#FFFAFA',
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    flexDirection: 'column',
+    paddingTop: 30,
+    paddingHorizontal: 20,
   },
   homeHeader: {
-    backgroundColor: "#FFFFFF",
+    flexDirection: 'row',
+    paddingVertical: 50, 
   },
+  Profile: {
+    width: 70,
+    height: 70,
+  },
+  headerText: {
+    paddingLeft: 20,
+    paddingRight: 90,
+  },
+  homeHeaderText: {
+    fontSize: 20,
+    paddingBottom: 5,
+    color: 'grey',
+    fontWeight: '300',
+  },
+  homeHeaderText2: {
+    fontSize: 28,
+    fontWeight: '500',
+  },
+  searchBackground: {
+    backgroundColor: '#F2F2F2',
+    borderRadius: 50,
+    borderWidth: 20,
+    borderColor: 'transparent',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  searchIcon: {
+    alignSelf: 'center',
+    //backgroundColor: '#B3B3B3',
+    padding: 5,
+    width: 30,
+    height: 20,
+  },
+  bankCard: {
+    height: 200,
+    width: 350,
+  },
+  card: {
+    width: 'auto',
+  },
+  status: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 50,
+  },
+  statusIcon: {
+    backgroundColor: '#F2F2F2',
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  transactionsHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  }, 
+  transactionsHeaderText: {
+    fontSize: 26,
+    fontWeight: '500'
+  },
+  transactionsHeaderLink: {
+    fontSize: 20,
+    color: '#4169E1',
+    fontWeight: '600'
+  }
 });
 
 export default styles;
